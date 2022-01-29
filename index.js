cbaf5c7e9d2c424fa09e0586feb4f38ebfb89f3a
Deocomate <deocomate@gmail.com>
2022-01-30 00:38:01 +0700

    first cm

---
+++ index.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $(".hopque img").click(function (e) { 
+        var ketqua = Math.floor(Math.random() * 10);
+        var outputP = document.getElementById("trave");
+        var manhinh = screen.width;
+        if (manhinh>799) {
+            console.log("May tinh");
+            outputP.style.fontSize="3vw";
+        } else {
+            console.log("Dien thoai")
+            outputP.style.fontSize="7vw";
+        }
+        
+        if (ketqua ==0) {
+            outputP.innerHTML="Ế"
+        } else if (ketqua==2){
+            outputP.innerHTML="Có NY nhưng 3 tháng ctay"
+        } else if (ketqua==3){
+            outputP.innerHTML="Có NY"
+        } else if (ketqua==4){
+            outputP.innerHTML="Ế dài hạn"
+        } else if (ketqua==5){
+            outputP.innerHTML="Ế nhưng kiếm dc nhiều tiền"
+        } else if (ketqua==6){
+            outputP.innerHTML="Có NY nhưng ko săn dc sale Shoppee"
+        } else if (ketqua==7){
+            outputP.innerHTML="Có NY (Cũ)"
+        } else if (ketqua==8){
+            outputP.innerHTML="Lấy chồng(vợ) không cần NY"
+        } else if (ketqua==9){
+            outputP.innerHTML="Độc thân vui tính"
+        }
+        
+
+
+
+
+    });
+});

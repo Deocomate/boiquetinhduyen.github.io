8ada5669df94b3e98f77670434483a1d65320c43
Deocomate <deocomate@gmail.com>
2022-01-30 10:45:59 +0700

    Responsive Web

--- index.js
+++ index.js
@@ -3,33 +3,44 @@ $(document).ready(function () {
         var ketqua = Math.floor(Math.random() * 10);
         var outputP = document.getElementById("trave");
         var manhinh = screen.width;
+        var hieuung = document.getElementById("animate");  
         if (manhinh>799) {
             console.log("May tinh");
             outputP.style.fontSize="3vw";
         } else {
             console.log("Dien thoai")
-            outputP.style.fontSize="5vw";
+            outputP.style.fontSize="5vw"; 
+        }
+        if (manhinh>799) {
+            traketqua();
+        }   else {
+            console.log("DT");
+            setTimeout(traketqua,1500);
+                                 
+        }
+        function traketqua(){
+            if (ketqua ==0) {
+            outputP.innerHTML="Ế"
+            } else if (ketqua==2){
+                outputP.innerHTML="Có NY nhưng 3 tháng ctay"
+            } else if (ketqua==3){
+                outputP.innerHTML="Có NY"
+            } else if (ketqua==4){
+                outputP.innerHTML="Ế dài hạn"
+            } else if (ketqua==5){
+                outputP.innerHTML="Ế nhưng kiếm dc nhiều tiền"
+            } else if (ketqua==6){
+                outputP.innerHTML="Có NY nhưng ko săn dc sale Shoppee"
+            } else if (ketqua==7){
+                outputP.innerHTML="Có NY (Cũ)"
+            } else if (ketqua==8){
+                outputP.innerHTML="Lấy chồng(vợ) không cần NY"
+            } else if (ketqua==9){
+                outputP.innerHTML="Độc thân vui tính"
+            }
+            hieuung.style.animation="none";
         }
         
-        if (ketqua ==0) {
-            outputP.innerHTML="Ế"
-        } else if (ketqua==2){
-            outputP.innerHTML="Có NY nhưng 3 tháng ctay"
-        } else if (ketqua==3){
-            outputP.innerHTML="Có NY"
-        } else if (ketqua==4){
-            outputP.innerHTML="Ế dài hạn"
-        } else if (ketqua==5){
-            outputP.innerHTML="Ế nhưng kiếm dc nhiều tiền"
-        } else if (ketqua==6){
-            outputP.innerHTML="Có NY nhưng ko săn dc sale Shoppee"
-        } else if (ketqua==7){
-            outputP.innerHTML="Có NY (Cũ)"
-        } else if (ketqua==8){
-            outputP.innerHTML="Lấy chồng(vợ) không cần NY"
-        } else if (ketqua==9){
-            outputP.innerHTML="Độc thân vui tính"
-        }
         
 
 
